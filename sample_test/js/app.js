b0eb680f4a630757f487c1b2e2e4aa3cee942bbd
Nataly <nat.khom@ukr.net>
2018-06-15 12:58:36 +0300

    Files updated

--- sample_test/js/app.js
+++ sample_test/js/app.js
@@ -5,7 +5,10 @@ function setValidator(id) {
                 required: true,
                 email: true
             },
-            firstname: "required",
+            firstname: {
+                required: true,
+                name_pattern: true
+            },
             phone: {
                 required: true,
                 digits: true
@@ -14,16 +17,25 @@ function setValidator(id) {
         messages: {
             email: {
                 required: "This field must be filled",
-                email: "The entered data are not correct"
+                email: "The entered data is not correct"
             },
-            firstname: "This field must be filled",
+            firstname: {
+                required: "This field must be filled",
+                name_pattern: "The username must contain only Latin letters"
+            },
             phone: {
                 required: "This field must be filled",
-                digits: "The entered data are not correct"
+                digits: "The entered data are is not correct"
             }
         }
     });
 }
+
+$(function () {
+    $.validator.addMethod("name_pattern", function (value, element) {
+        return this.optional(element) || /(?=.*[a-zA-Z])/.test(value);
+    });
+});
 
 $(document).ready(function () {
     setValidator($('#access-form'));
